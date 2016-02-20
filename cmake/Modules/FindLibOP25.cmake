3427b6c1c218d3d64a5dfd56ace71a4f3a07cfcb
robotastic <luke@robotastic.com>
2016-02-20 14:32:45 -0500

    found it

--- cmake/Modules/FindLibOP25.cmake
+++ cmake/Modules/FindLibOP25.cmake
@@ -3,18 +3,23 @@ if(NOT LIBOP25_FOUND)
   pkg_check_modules (LIBOP25_PKG libop25)
   find_path(LIBOP25_INCLUDE_DIR
     NAMES op25/api.h
-    HINTS ${LIBOP25_PKG_INCLUDE_DIRS}
-    PATHS /usr/include
-          /usr/local/include
-          /home/luke/gr3.7/include
+    HINTS $ENV{GNURADIO_RUNTIME_DIR}/include
+          ${PC_GNURADIO_RUNTIME_INCLUDE_DIRS}
+          ${CMAKE_INSTALL_PREFIX}/include
+    PATHS /usr/local/include
+          /usr/include
   )
 
   find_library(LIBOP25_LIBRARIES
     NAMES gnuradio-op25
-    HINTS ${LIBOP25_PKG_LIBRARY_DIRS}
-    PATHS /usr/lib
-          /usr/local/lib
-          /home/luke/gr3.7/lib
+    HINTS $ENV{GNURADIO_RUNTIME_DIR}/lib
+          ${PC_GNURADIO_RUNTIME_LIBDIR}
+          ${CMAKE_INSTALL_PREFIX}/lib/
+          ${CMAKE_INSTALL_PREFIX}/lib64/
+    PATHS /usr/local/lib
+          /usr/local/lib64
+          /usr/lib
+          /usr/lib64
   )
 message(STATUS "Pkg: ${LIBOP25_PKG}, ${LIBOP25_PKG_INCLUDE_DIRS}, ${LIBOP25_PKG_LIBRARY_DIRS}")
 message(STATUS "Vars: ${LIBOP25_INCLUDE_DIR}, ${LIBOP25_LIBRARIES}")
