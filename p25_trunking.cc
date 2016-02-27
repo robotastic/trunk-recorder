e8b6e0167e8a0c5a816c2b07fcfc1b18c96f6de8
robotastic <luke@robotastic.com>
2016-02-26 23:03:29 -0500

    QPSK for everyone!

--- p25_trunking.cc
+++ p25_trunking.cc
@@ -9,109 +9,136 @@ p25_trunking_sptr make_p25_trunking(double freq, double center, long s,  gr::msg
 }
 
 
-unsigned p25_trunking::GCD(unsigned u, unsigned v) {
-	while ( v != 0) {
-		unsigned r = u % v;
-		u = v;
-		v = r;
-	}
-	return u;
-}
-
-std::vector<float> p25_trunking::design_filter(double interpolation, double deci) {
-	float beta = 5.0;
-	float trans_width = 0.5 - 0.4;
-	float mid_transition_band = 0.5 - trans_width/2;
-
-	std::vector<float> result = gr::filter::firdes::low_pass(
-	                                interpolation,
-	                                1,
-	                                mid_transition_band/interpolation,
-	                                trans_width/interpolation,
-	                                gr::filter::firdes::WIN_KAISER,
-	                                beta
-	                            );
-
-	return result;
-}
-
-
 
 p25_trunking::p25_trunking(double f, double c, long s, gr::msg_queue::sptr queue)
 	: gr::hier_block2 ("p25_trunking",
 	                   gr::io_signature::make  (1, 1, sizeof(gr_complex)),
 	                   gr::io_signature::make  (0, 0, sizeof(float)))
 {
-	freq = f;
+        
+        	freq = f;
 	center = c;
-	long capture_rate = s;
-
-
+	long samp_rate = s;
 
 	float offset = freq - center;
 
 
 
-	float symbol_rate = 4800;
+    float symbol_rate = 4800;
 	double samples_per_symbol = 10;
 	double system_channel_rate = symbol_rate * samples_per_symbol;
-	double symbol_deviation = 600.0;
-	double prechannel_decim = floor(capture_rate / system_channel_rate);
-	double prechannel_rate = capture_rate / prechannel_decim;
-	double trans_width = 12500 / 2;
-	double trans_centre = trans_width + (trans_width / 2);
+	float symbol_deviation = 600.0;
+	bool fsk4 = false;
 
 	std::vector<float> sym_taps;
 	const double pi = M_PI; //boost::math::constants::pi<double>();
 
-	timestamp = time(NULL);
-	starttime = time(NULL);
+
+        
+        float if_rate = 24000;
+        float gain_mu = 0.025;
+        float costas_alpha = 0.04;
+        float bb_gain = 1.0;
+
+       	baseband_amp = gr::blocks::multiply_const_ff::make(bb_gain);
 
 
 
-	prefilter = gr::filter::freq_xlating_fir_filter_ccf::make(int(prechannel_decim),
-	            gr::filter::firdes::low_pass(1.0, capture_rate, trans_centre,trans_width, gr::filter::firdes::WIN_HANN),  //trans_centre, trans_width, gr::filter::firdes::WIN_HANN),
+ 	float xlate_bandwidth = 14000; //14000; //24260.0
+
+        
+            lpf_coeffs = gr::filter::firdes::low_pass(1.0, samp_rate, xlate_bandwidth/2, 1500, gr::filter::firdes::WIN_HANN);
+        int decimation = int(samp_rate / if_rate);
+       
+        prefilter = gr::filter::freq_xlating_fir_filter_ccf::make(decimation,
+	            lpf_coeffs,
 	            offset,
-	            capture_rate);
+	            samp_rate);
 
-	//int squelch_db = 40;
-	// squelch = gr::analog::pwr_squelch_cc::make(squelch_db, 0.001, 0, true);
-	BOOST_LOG_TRIVIAL(info) << "Prechannel Decim: " << floor(capture_rate / system_channel_rate) << " Rate: " << prechannel_rate << " system_channel_rate: " << system_channel_rate;
-
-	unsigned int d = GCD(prechannel_rate, system_channel_rate);
-	double small_system_channel_rate = floor(system_channel_rate  / d);
-	double small_prechannel_rate = floor(prechannel_rate / d);
-	BOOST_LOG_TRIVIAL(info) << "After GCD - Prechannel Decim: " << prechannel_decim << " Rate: " << small_prechannel_rate << " system_channel_rate: " << small_system_channel_rate;
+        float resampled_rate = float(samp_rate) / float(decimation); // rate at output of self.lpf
+        float arb_rate = (float(if_rate) / resampled_rate);
+        float arb_size = 32;
+        float arb_atten=100;
 
 
-	resampler_taps = design_filter(small_system_channel_rate, small_prechannel_rate);
+            // Create a filter that covers the full bandwidth of the output signal
 
-	downsample_sig = gr::filter::rational_resampler_base_ccf::make(small_system_channel_rate, small_prechannel_rate, resampler_taps);
-	//resampler_taps = design_filter(small_prechannel_rate, small_system_channel_rate);
+            // If rate >= 1, we need to prevent images in the output,
+            // so we have to filter it to less than half the channel
+            // width of 0.5.  If rate < 1, we need to filter to less
+            // than half the output signal's bw to avoid aliasing, so
+            // the half-band here is 0.5*rate.
+            float percent = 0.80;
+            if(arb_rate < 1) {
+                float halfband = 0.5* arb_rate;
+                float bw = percent*halfband;
+                float tb = (percent/2.0)*halfband;
 
-	//downsample_sig = gr::filter::pfb_arb_resampler_ccf::make(float(system_channel_rate) / float(prechannel_rate));
+
+                // As we drop the bw factor, the optfir filter has a harder time converging;
+                // using the firdes method here for better results.
+                arb_taps = gr::filter::firdes::low_pass_2(arb_size, arb_size, bw, tb, arb_atten,
+                                                      gr::filter::firdes::WIN_BLACKMAN_HARRIS);
+            } else {
+                BOOST_LOG_TRIVIAL(error) << "Something is probably wrong! Resampling rate too low";
+                exit(0);
+            }
 
 
-	double fm_demod_gain = floor(system_channel_rate / (2.0 * pi * symbol_deviation));
-	demod = gr::analog::quadrature_demod_cf::make(fm_demod_gain);
+
+
+
+
+        arb_resampler = gr::filter::pfb_arb_resampler_ccf::make(arb_rate, arb_taps );
+
+
+        agc = gr::analog::feedforward_agc_cc::make(16, 1.0);
+
+
+        float omega = float(if_rate) / float(symbol_rate);
+        float gain_omega = 0.1  * gain_mu * gain_mu;
+
+        float alpha = costas_alpha;
+        float beta = 0.125 * alpha * alpha;
+        float fmax = 2400;	// Hz
+        fmax = 2*pi * fmax / float(if_rate);
+
+        costas_clock = gr::op25_repeater::gardner_costas_cc::make(omega, gain_mu, gain_omega, alpha,  beta, fmax, -fmax);
+
+
+        // Perform Differential decoding on the constellation
+        diffdec = gr::digital::diff_phasor_cc::make();
+
+        // take angle of the difference (in radians)
+        to_float = gr::blocks::complex_to_arg::make();
+
+        // convert from radians such that signal is in -3/-1/+1/+3
+        rescale = gr::blocks::multiply_const_ff::make( (1 / (pi / 4)) );
+
+        // fm demodulator (needed in fsk4 case)
+        float fm_demod_gain = if_rate / (2.0 * pi * symbol_deviation);
+        fm_demod = gr::analog::quadrature_demod_cf::make(fm_demod_gain);
+
+
 
 	double symbol_decim = 1;
 
-	BOOST_LOG_TRIVIAL(info) << " FM Gain: " << fm_demod_gain << " PI: " << pi << " Samples per sym: " << samples_per_symbol;
+
 
 	for (int i=0; i < samples_per_symbol; i++) {
 		sym_taps.push_back(1.0 / samples_per_symbol);
 	}
-	//symbol_coeffs = (1.0/samples_per_symbol,)*samples_per_symbol
+
 	sym_filter =  gr::filter::fir_filter_fff::make(symbol_decim, sym_taps);
-	tune_queue = gr::msg_queue::make(100);
+	tune_queue = gr::msg_queue::make(2);
+	traffic_queue = gr::msg_queue::make(2);
 	rx_queue = queue;
 	const float l[] = { -2.0, 0.0, 2.0, 4.0 };
 	std::vector<float> levels( l,l + sizeof( l ) / sizeof( l[0] ) );
-	op25_demod = gr::op25::fsk4_demod_ff::make(tune_queue, system_channel_rate, symbol_rate);
-	op25_slicer = gr::op25_repeater::fsk4_slicer_fb::make(levels);
+	fsk4_demod = gr::op25::fsk4_demod_ff::make(tune_queue, system_channel_rate, symbol_rate);
+	slicer = gr::op25_repeater::fsk4_slicer_fb::make(levels);
 
-	int udp_port = 0;
+    int udp_port = 0;
 	int verbosity = 0;
 	const char * wireshark_host="127.0.0.1";
 	bool do_imbe = 0;
@@ -120,23 +147,31 @@ p25_trunking::p25_trunking(double f, double c, long s, gr::msg_queue::sptr queue
 	bool do_audio_output = 0;
 	bool do_tdma = 0;
 	op25_frame_assembler = gr::op25_repeater::p25_frame_assembler::make(wireshark_host,udp_port,verbosity,do_imbe, do_output, do_msgq, rx_queue, do_audio_output, do_tdma);
-	//op25_vocoder = gr::op25_repeater::vocoder::make(0, 0, 0, "", 0, 0);
-
-	converter = gr::blocks::short_to_float::make();
 
 
 
 
-	connect(self(),0, prefilter,0);
-	connect(prefilter, 0, downsample_sig, 0);
-	connect(downsample_sig, 0, demod, 0);
-	connect(demod, 0, sym_filter, 0);
-	connect(sym_filter, 0, op25_demod, 0);
-	connect(op25_demod,0, op25_slicer, 0);
-
-	connect(op25_slicer,0, op25_frame_assembler,0);
-
-
+	if (fsk4) {
+        connect(self(),0, prefilter,0);
+		connect(prefilter,0, arb_resampler, 0);
+		connect(arb_resampler,0, fm_demod,0);
+		connect(fm_demod, 0, baseband_amp, 0);
+		connect(baseband_amp,0, sym_filter, 0);
+		connect(sym_filter, 0, fsk4_demod, 0);
+		connect(fsk4_demod, 0, slicer, 0);
+		connect(slicer,0, op25_frame_assembler,0);
+	} else {
+        connect(self(),0, prefilter,0);
+		connect(prefilter, 0, arb_resampler, 0);
+		connect(arb_resampler,0, agc,0);
+		connect(agc, 0, costas_clock, 0);
+		connect(costas_clock,0, diffdec, 0);
+		connect(diffdec, 0, to_float, 0);
+		connect(to_float,0, rescale, 0);
+		connect(rescale, 0, slicer, 0);
+		connect(slicer,0, op25_frame_assembler,0);
+	}
+        
 }
 
 
@@ -145,25 +180,8 @@ p25_trunking::~p25_trunking() {
 }
 
 
-bool p25_trunking::is_active() {
-	return active;
-}
-
-
 double p25_trunking::get_freq() {
 	return freq;
-}
-
-char *p25_trunking::get_filename() {
-	return filename;
-}
-
-int p25_trunking::lastupdate() {
-	return time(NULL) - timestamp;
-}
-
-long p25_trunking::elapsed() {
-	return time(NULL) - starttime;
 }
 
 
@@ -172,44 +190,6 @@ void p25_trunking::tune_offset(double f) {
 	int offset_amount = (f - center);
 	prefilter->set_center_freq(offset_amount); // have to flip this for 3.7
 	//BOOST_LOG_TRIVIAL(info) << "Offset set to: " << offset_amount << " Freq: "  << freq;
-}
-
-void p25_trunking::deactivate() {
-	BOOST_LOG_TRIVIAL(info) << "logging_receiver_dsd.cc: Deactivating Logger [ " << num << " ] - freq[ " << freq << "] \t talkgroup[ " << talkgroup << " ]";
-
-	active = false;
-	valve->set_enabled(false);
-	wav_sink->close();
-
-}
-
-void p25_trunking::activate(long t, double f, int n) {
-
-	timestamp = time(NULL);
-	starttime = time(NULL);
-
-	talkgroup = t;
-	freq = f;
-
-	tm *ltm = localtime(&starttime);
-	BOOST_LOG_TRIVIAL(info) << "logging_receiver_dsd.cc: Activating Logger [ " << num << " ] - freq[ " << freq << "] \t talkgroup[ " << talkgroup << " ]";
-
-	prefilter->set_center_freq(f - center); // have to flip for 3.7
-
-
-
-	std::stringstream path_stream;
-	path_stream << boost::filesystem::current_path().string() <<  "/" << 1900 + ltm->tm_year << "/" << 1 + ltm->tm_mon << "/" << ltm->tm_mday;
-
-	boost::filesystem::create_directories(path_stream.str());
-	sprintf(filename, "%s/%ld-%ld_%g.wav", path_stream.str().c_str(),talkgroup,timestamp,f);
-
-
-
-	wav_sink->open(filename);
-	active = true;
-	valve->set_enabled(true);
-
 }
 
 
