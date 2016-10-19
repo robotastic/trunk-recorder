835b85ad0018c3bd30e5095c6fb64581f921a9b1
robotastic <luke@robotastic.com>
2016-10-19 07:09:40 -0400

    wider initial downsample

--- trunk-recorder/recorders/p25_recorder.cc
+++ trunk-recorder/recorders/p25_recorder.cc
@@ -55,10 +55,11 @@ p25_recorder::p25_recorder(Source *src, bool qpsk)
   valve = gr::blocks::copy::make(sizeof(gr_complex));
   valve->set_enabled(false);
 
+  lpf_coeffs = gr::filter::firdes::low_pass(1.0, input_rate, 15000, 1500, gr::filter::firdes::WIN_HANN);
 
-  lpf_coeffs = gr::filter::firdes::low_pass(1.0, input_rate, xlate_bandwidth / 2, 3000, gr::filter::firdes::WIN_HAMMING, 6.76);
-  int decimation = int(input_rate / system_channel_rate);
-
+//  lpf_coeffs = gr::filter::firdes::low_pass(1.0, input_rate, xlate_bandwidth / 2, 3000, gr::filter::firdes::WIN_HANN);
+  //int decimation = int(input_rate / system_channel_rate);
+int decimation = int(input_rate / (system_channel_rate*2));
 
   std::vector<gr_complex> dest(lpf_coeffs.begin(), lpf_coeffs.end());
 
