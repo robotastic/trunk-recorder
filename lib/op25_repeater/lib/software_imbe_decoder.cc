79a8d02d42dc5804aaa4fb4767a8d4d196e6edde
robotastic <luke@robotastic.com>
2017-05-29 14:22:56 -0400

    better balance

--- lib/op25_repeater/lib/software_imbe_decoder.cc
+++ lib/op25_repeater/lib/software_imbe_decoder.cc
@@ -31,6 +31,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <stdint.h>
+#include <iostream>
 #include <unistd.h>
 #include <fcntl.h>
 #include <math.h>
@@ -958,8 +959,9 @@ software_imbe_decoder::decode_tap(int _L, int _K, float _w0, const int * _v, con
          // Most of the difference is compensated by removing the 146.6433 factor
          // in the synth_unvoiced procedure.  The final tweak is done by raising the
          // voiced samples:
-				 float sample = (suv[en]/4) + sv[en]; //balance v/uv loudness
+				 float sample = (suv[en]) + sv[en]*3; //balance v/uv loudness
          if(abs((int)sample) > 32767) {
+					 	std::cout << "Sample over: " << sample << std::endl;
             sample = (sample < 0) ? -32767 : 32767; // * sgn(sample)
          }
          samples->push_back(sample);
