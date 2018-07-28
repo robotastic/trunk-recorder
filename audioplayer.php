2307be50057de5b3affd3529c59a8d8df63d5ae6
Mark Tomlin <Dygear@gmail.com>
2018-07-28 02:19:16 -0400

    Now reads config.json file.
    * Now reads config.json file.
    * Fixed the small UI bug where it would always highlight the top row.
    * The Javascript was rewritten from the ground up.
    * You can now select the file type you wish to play and monitor at the top of the file.

--- audioplayer.php
+++ audioplayer.php
@@ -1,40 +1,63 @@
-<?php 
-$TGS = [
-    1767 => 'Example1',
-    1777 => 'Example2'
-];
+<?php
+$FileType = '.wav';
+
+$system = (empty($_GET['system'])) ? NULL : $_GET['system'];
+$date   = (isset($_GET['date'])) ? new DateTimeImmutable($_GET['date']) : new DateTimeImmutable();
+$tg     = (empty($_GET['tg'])) ? NULL : $_GET['tg'];
+
+$CONFIG = (function (string $configFilePath = './config.json') {
+    if (!file_exists($configFilePath))
+        return false;
+
+    return json_decode(file_get_contents($configFilePath));
+})();
+
+$TGFile = function (string $tgFilePath): array {
+    $return = [];
+
+    if (!file_exists($tgFilePath))
+        return $return;
+
+    foreach (file($tgFilePath) as $line) {
+        [$DEC, $HEX, $Mode, $AlphaTag, $Description, $Tag, $Group, $Priority] = str_getcsv($line);
+        $return[$DEC] = $AlphaTag;
+    }
+
+    return $return;
+};
+
+$TGS = [];
+foreach ($CONFIG->systems as $system) {
+    $TGS += $TGFile($system->talkgroupsFile);
+}
 
 $MHz = function (float $MHz): string {
-    switch (TRUE)
-    {
+    switch (TRUE) {
         case $MHz >= 136 AND $MHz <= 174:
-            return 'bg-primary';
+            return 'bg-primary'; # VHF
         case $MHz >= 380 AND $MHz <= 520:
-            return 'bg-danger';
+            return 'bg-danger';  # UHF
         case $MHz >= 764 AND $MHz <= 870:
-            return 'bg-warning';
+            return 'bg-warning'; # 700
         case $MHz >= 896 AND $MHz <= 901:
         case $MHz >= 935 AND $MHz <= 940:
-            return 'bg-success';
+            return 'bg-success'; # 800
         default:
             return 'null';
     }
 };
 
-$files     = [];
-$date      = (isset($_GET['date'])) ? new DateTimeImmutable($_GET['date']) : new DateTimeImmutable();
-$TG        = (empty($_GET['TG'])) ? NULL : $_GET['TG'];
-
+$files = [];
 try  {
-    $path = new DirectoryIterator('./audio/' . $date->format('Y/n/j') . '/');
+    $path = new DirectoryIterator("{$CONFIG->captureDir}/{$date->format('Y/n/j')}/");
 
-    if (!$path->isDir())
+    if (!$path->isDir()) {
         throw new Exception('No directory found for that date.');
+    }
 
-    foreach ($path as $file)
-    {
+    foreach ($path as $file) {
         $EXT = '.' . $file->getExtension();
-        if (in_array($EXT, ['.m4a', '.wav']))
+        if ($EXT != $FileType)
             continue;
 
         $Basename = $file->getBasename($EXT);
@@ -43,11 +66,10 @@ try  {
             continue;
 
         [$TGID, $TIME, $FREQ] = preg_split('/[-_]/', $Basename);
-        substr($FREQ, 0, -4);
-        if ($TGID != $TG AND $TG !== NULL)
+        if ($TGID != $tg AND $tg !== NULL)
             continue;
 
-        $files[$file->getMTime()] = [$file->getFilename(), $file->getSize(), $TGID, $TIME, $FREQ];
+        $files[$file->getMTime()] = [$Basename, round($file->getSize() / 1024), $TGID, $TIME, $FREQ / 1000000];
     }
 
     ksort($files);
@@ -56,6 +78,7 @@ try  {
     $error = $e->getMessage();
 }
 
+$path = substr(realpath($CONFIG->captureDir), strlen($_SERVER['DOCUMENT_ROOT']));
 
 ?>
 <!DOCTYPE html>
@@ -67,48 +90,7 @@ try  {
     </head>
     <body>
         <div class="container">
-            <script>
-                window.onload = function () { 
-                    var index = 0,
-                      sources = document.getElementsByTagName('source')[0],
-                        audio = document.getElementsByTagName('audio')[0],
-                         list = document.getElementsByTagName('tbody')[0];
-
-                    list.addEventListener('click', function (e) {
-                        var mp3File = e.target.parentElement.getElementsByTagName('a')[0];
-                        for (var item = 0; item < list.getElementsByTagName("a").length; item++) {
-                            if (list.getElementsByTagName("a")[item] == mp3File) {
-                                list.getElementsByTagName("tr")[index].classList.toggle("text-attention");
-                                index = item;   // Update the index to the item we clicked on.
-                                list.getElementsByTagName("tr")[index].classList.toggle("text-attention");
-                                sources.setAttribute('src', mp3File);
-                                audio.load();
-                                audio.play();
-                            }
-                        }
-                    }, false);
-
-                    audio.addEventListener('ended',function () {
-                        if (index == (list.getElementsByTagName('tr').length - 1))
-                            return;
-
-                        list.getElementsByTagName("tr")[index].classList.toggle("text-attention");
-                        index++;
-                        var mp3File = list.getElementsByTagName('tr')[index].getElementsByTagName('a')[0];
-                        sources.setAttribute('src', mp3File);
-                        list.getElementsByTagName("tr")[index].classList.toggle("text-attention");
-                        audio.load();
-                        audio.play();
-                    }, false);
-                }
-            </script>
-            <style>
-                #interface {
-                    width: 100%;
-                }
-            </style>
             <div id="interface">
-                <h1><?=(isset($TGS[$TG]) AND ($TG !== NULL)) ? $TGS[$TG] : "All Calls"?> on <?=$date->format('Y-m-d')?></h1>
                 <form method="get">
                     <div class="row">
                         <div class="form-group col-lg-4">
@@ -116,24 +98,23 @@ try  {
                             <input class="form-control" id="date" name="date" type="date" value="<?=$date->format('Y-m-d')?>" />
                         </div>
                         <div class="form-group col-lg-4">
-                            <label class="form-control-label" for="TG">Talk Group</label>
-                            <select class="form-control" id="TG" name="TG">
+                            <label class="form-control-label" for="tg">Talk Group</label>
+                            <select class="form-control" id="tg" name="tg">
                                 <option value="">All Calls</option>
 <?php       foreach ($TGS as $TGID => $TGName): ?>
-                                <option value="<?=$TGID?>"<?=($TG == $TGID) ? ' selected="selected"' : ''?>><?=$TGName?></option>
+                                <option value="<?=$TGID?>"<?=($tg == $TGID) ? ' selected="selected"' : ''?>><?=$TGName?></option>
 <?php       endforeach; ?>
                             </select>
                         </div>
                         <div class="form-group col-lg-4">
                             <label class="form-control-label">Controls</label>
-                            <button class="btn btn-success btn-block" type="submit">View</button>
+                            <button class="btn btn-outline-success btn-block" type="submit">View</button>
                         </div>
                     </div>
                 </form>
                 <div class="row">
                     <div class="form-group col-lg-12">
-                        <audio preload="none" tabindex="0" controls>
-                            <source type="audio/wav" src="http://www.lalarm.com/en/images/silence.wav" />
+                        <audio preload="none" controls>
                             Sorry, your browser does not support HTML5 audio.
                         </audio>
                     </div>
@@ -159,12 +140,73 @@ try  {
                     <tr>
                         <td><?=date("H:i:s", $FileTime)?></td>
                         <td><?=($TGS[$TGID]) ?? $TGID?></td>
-                        <td class="<?=$MHz($FREQ / 1000000)?>"><?=sprintf("%3.6f", (float) $FREQ / 1000000)?></td>
-                        <td><a href="audio/<?=$date->format('Y/n/j') . '/' . $FileName?>"><?=round($FileSize / 1024)?>k</a></td>
+                        <td class="<?=$MHz($FREQ)?>"><?=sprintf("%3.6f", $FREQ)?></td>
+                        <td><a href="<?="{$path}/{$date->format('Y/n/j')}/{$FileName}{$FileType}"?>"><?=$FileSize?>k</a></td>
                     </tr>
 <?php   endforeach; ?>
-                </tbody>
             </table>
         </div>
+        <script>
+            window.onload = _ => {
+                var index = null,
+                  sources = document.querySelector('source'),
+                    audio = document.querySelector('audio'),
+                     list = document.querySelector('tbody'),
+                       tr = [...list.getElementsByTagName('tr')];
+
+                list.addEventListener('click', e => {
+                    if (tr[index])
+                        tr[index].classList.toggle('bg-secondary');
+
+                    tr.forEach((thisRow, i) => {
+                        if (e.target.parentElement == thisRow) {
+                            index = i;
+                            tr[index].classList.toggle('bg-secondary');
+                        }
+                    });
+
+                    audio.src = tr[index].querySelector('a');
+                    audio.load();
+                    audio.play().catch(error => {
+                        console.log(`${error.name}: ${error.message}`);
+                    });
+                }, false);
+
+                audio.addEventListener('ended', _ => {
+                    tr[index++].classList.toggle('bg-secondary');
+                    if (tr[index])
+                        tr[index].click();
+                }, false);
+            }
+        </script>
+        <style>
+            html, body {
+                background: #012;
+                color: #FFF;
+            }
+            #interface, audio {
+                width: 100%;
+            }
+            table {
+                text-align: center;
+            }
+            input, select, textarea {
+                color: #fff !important;
+                background-color: #1a1c22 !important;
+                border-color: #434857 !important;
+            }
+            tbody tr {
+                cursor: pointer;
+            }
+            a {
+                color: #FA0;
+            }
+            a:hover {
+                color: #FC0;
+            }
+            a:active {
+                color: #FF0;
+            }
+        </style>
     </body>
 </html>
