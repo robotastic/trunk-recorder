e3c4ce898fb7cab57abaac08d4a32eca732c7d1d
leee <leee@users.noreply.github.com>
2020-07-29 16:12:59 -0400

    Make OSW constants 12-bit and not 0-padded 16-bit for readability

--- trunk-recorder/systems/smartnet_parser.h
+++ trunk-recorder/systems/smartnet_parser.h
@@ -12,28 +12,28 @@
 
 #include <boost/log/trivial.hpp>
 
-#define OSW_BACKGROUND_IDLE     0x02f8
-#define OSW_FIRST_CODED_PC      0x0304
-#define OSW_FIRST_NORMAL        0x0308
-#define OSW_FIRST_TY2AS1        0x0309
-#define OSW_EXTENDED_FCN        0x030b
-#define OSW_AFFIL_FCN           0x030d
-#define OSW_TY2_AFFILIATION     0x0310
-#define OSW_TY1_STATUS_MIN      0x0310
-#define OSW_TY2_MESSAGE         0x0311
-#define OSW_TY1_STATUS_MAX      0x0317
-#define OSW_TY1_ALERT           0x0318
-#define OSW_TY1_EMERGENCY       0x0319
-#define OSW_TY2_CALL_ALERT      0x0319
-#define OSW_FIRST_ASTRO         0x0321
-#define OSW_SYSTEM_CLOCK        0x0322
-#define OSW_SCAN_MARKER         0x032b
-#define OSW_EMERG_ANNC          0x032e
-#define OSW_AMSS_ID_MIN         0x0360
-#define OSW_AMSS_ID_MAX         0x039f
-#define OSW_CW_ID               0x03a0
-#define OSW_SYS_NETSTAT         0x03bf
-#define OSW_SYS_STATUS          0x03c0
+#define OSW_BACKGROUND_IDLE     0x2f8
+#define OSW_FIRST_CODED_PC      0x304
+#define OSW_FIRST_NORMAL        0x308
+#define OSW_FIRST_TY2AS1        0x309
+#define OSW_EXTENDED_FCN        0x30b
+#define OSW_AFFIL_FCN           0x30d
+#define OSW_TY2_AFFILIATION     0x310
+#define OSW_TY1_STATUS_MIN      0x310
+#define OSW_TY2_MESSAGE         0x311
+#define OSW_TY1_STATUS_MAX      0x317
+#define OSW_TY1_ALERT           0x318
+#define OSW_TY1_EMERGENCY       0x319
+#define OSW_TY2_CALL_ALERT      0x319
+#define OSW_FIRST_ASTRO         0x321
+#define OSW_SYSTEM_CLOCK        0x322
+#define OSW_SCAN_MARKER         0x32b
+#define OSW_EMERG_ANNC          0x32e
+#define OSW_AMSS_ID_MIN         0x360
+#define OSW_AMSS_ID_MAX         0x39f
+#define OSW_CW_ID               0x3a0
+#define OSW_SYS_NETSTAT         0x3bf
+#define OSW_SYS_STATUS          0x3c0
 
 struct osw_stru{
 	unsigned short cmd;
