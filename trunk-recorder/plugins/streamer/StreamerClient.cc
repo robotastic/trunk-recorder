c496b75e9bc2d00e25e529759aa1b97006c3743c
Joe Gilkey <JoeGilkey@users.noreply.github.com>
2020-11-17 15:46:55 -0500

    :(

--- trunk-recorder/plugins/streamer/StreamerClient.cc
+++ trunk-recorder/plugins/streamer/StreamerClient.cc
@@ -8,13 +8,14 @@ using streamer::TrunkRecorderStreamer;
 
 StreamerClient::StreamerClient(std::shared_ptr<::grpc::Channel> channel, bool enable_audio_streams)
     : stub_(::streamer::TrunkRecorderStreamer::NewStub(channel)),
-      m_enable_audio_streams(enable_audio_streams) {
-          signal_writer_ = stub_->SendSignal(&signal_context_, &signal_response_);
-          if(m_enable_audio_streams) {
-              audio_writer_ = stub_->SendStream(&audio_context_, &audio_response_);
-          }
-      }
-~StreamerClient::StreamerClient() {
+{
+    m_enable_audio_streams = enable_audio_streams;
+    signal_writer_ = stub_->SendSignal(&signal_context_, &signal_response_);
+    if(m_enable_audio_streams) {
+        audio_writer_ = stub_->SendStream(&audio_context_, &audio_response_);
+    }
+}
+StreamerClient::~StreamerClient() {
     signal_writer_->WritesDone();
     if(m_enale_audio_streams) {
         audio_writer_->WriteDone();
