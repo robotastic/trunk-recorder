0527215d880b899b33e5215cb4dec89bf76bb781
Jason McHuff <github@jasonmchuff.net>
2021-03-13 00:39:20 -0800

    moving unit-script.sh description to separate file due to length and use

--- examples/unit-script.sh
+++ examples/unit-script.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
-CAPTUREDIR="" #fill this in with the path used in config.json, no ending /
+#README: unit-script.txt
 
-# Creates a list of radio IDs in a file named "radiolist.csv" located in the
-# shortName directory along side the recordings, and logs radio activity to
-# "radiolog.csv" files located in each day's recordings directory
-
-# file format: radioID,timestamp,action,talkgroup,
-# for radiolist.csv, acknowledgment response timestamps are added at the end
-# when they are seen after a different action
-
-# Feel free to customize this script; to use for multiple systems, include in
-# each system's config.json section
-
-# NOTE: You need to run "echo > radiolist.csv" where the file(s) is going to be
-# beforehand as sed doesn't work on empty files, and to capture actions before
-# trunk-recorder makes the daily directory upon recording the first call, set
-# up a cron task of: 0 0 * * * mkdir -p <capturedir>/$(date +\%Y/\%-m/\%-d/)
-
-# sed usage based on https://stackoverflow.com/a/49852337
+CAPTUREDIR=""
 
 printf -v TRDATE '%(%Y/%-m/%-d)T'
 printf -v NOWTIME '%(%s)T'
