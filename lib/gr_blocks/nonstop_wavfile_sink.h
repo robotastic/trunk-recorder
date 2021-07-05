93b856bbc8716842e7ca0b3ff21e6e9892da6e2a
Luke Berndt <luke@robotastic.com>
2021-07-05 10:24:13 -0400

    Update nonstop_wavfile_sink.h

--- lib/gr_blocks/nonstop_wavfile_sink.h
+++ lib/gr_blocks/nonstop_wavfile_sink.h
@@ -24,6 +24,7 @@
 #define INCLUDED_GR_NONSTOP_WAVFILE_SINK_H
 
 #include "../trunk-recorder/call.h"
+#include "../trunk-recorder/recorders/recorder.h"
 //#include "../trunk-recorder/call_conventional.h"
 #include <boost/log/trivial.hpp>
 #include <gnuradio/blocks/api.h>
