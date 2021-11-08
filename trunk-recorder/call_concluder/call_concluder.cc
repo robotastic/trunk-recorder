b8ff5a5bd89e85e1e0a3642af13bb57ffd6a8c76
Luke Berndt <lukekb@gmail.com>
2021-11-08 16:44:27 -0500

    Update call_concluder.cc
    Fixed JSON error

--- trunk-recorder/call_concluder/call_concluder.cc
+++ trunk-recorder/call_concluder/call_concluder.cc
@@ -65,7 +65,7 @@ int create_call_json(Call_Data_t call_info) {
     json_file << "\"call_length\": " << call_info.length << ",\n";
     //json_file << "\"source\": \"" << this->get_recorder()->get_source()->get_device() << "\",\n";
     json_file << "\"talkgroup\": " << call_info.talkgroup << ",\n";
-    json_file << "\"talkgroup_tag\": " << call_info.talkgroup_tag << ",\n";
+    json_file << "\"talkgroup_tag\": \"" << call_info.talkgroup_tag << "\",\n";
     json_file << "\"freqList\": [";
     json_file << "{ \"freq\": " << std::fixed << std::setprecision(0) << call_info.freq << ", \"time\": " << call_info.start_time << ", \"pos\": 0.0, \"len\": " << call_info.length << ", \"error_count\": 0.0, \"spike_count\": 0.0}";
     json_file << "],\n";
