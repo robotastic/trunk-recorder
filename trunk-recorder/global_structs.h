20149f4bd9bbf9b0568234fc8a5819ac53ab117f
Luke Berndt <lukekb@gmail.com>
2021-11-28 08:48:05 -0500

    changed documentation

--- trunk-recorder/global_structs.h
+++ trunk-recorder/global_structs.h
@@ -19,7 +19,6 @@ struct Config {
   std::string upload_script;
   std::string upload_server;
   std::string bcfy_calls_server;
-  std::string rdioscanner_server;
   std::string status_server;
   std::string instance_key;
   std::string instance_id;
