5798d993afbdabdd49fbf4298d1d6477fecdd51e
Luke Berndt <lukekb@gmail.com>
2021-12-15 07:37:44 -0500

    Fixed JSON error & added shortname

--- trunk-recorder/call_concluder/call_concluder.cc
+++ trunk-recorder/call_concluder/call_concluder.cc
@@ -66,7 +66,8 @@ int create_call_json(Call_Data_t call_info) {
     //json_file << "\"source\": \"" << this->get_recorder()->get_source()->get_device() << "\",\n";
     json_file << "\"talkgroup\": " << call_info.talkgroup << ",\n";
     json_file << "\"talkgroup_tag\": \"" << call_info.talkgroup_tag << "\",\n";
-    json_file << "\"audio_type\": " << call_info.audio_type << ",\n";
+    json_file << "\"audio_type\": \"" << call_info.audio_type << "\",\n";
+    json_file << "\"short_name\": \"" << call_info.short_name << "\",\n";
     if (call_info.patched_talkgroups.size()>1){
       json_file << "\"patched_talkgroups\": [";
       bool first = true;
