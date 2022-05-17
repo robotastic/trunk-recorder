4a6aef551525b31927821738f670fde1105ff13b
Luke Berndt <lukekb@gmail.com>
2022-05-16 21:58:41 -0400

    Created better Public API
    Made it easier to have a public API

--- trunk-recorder/recorders/debug_recorder.h
+++ trunk-recorder/recorders/debug_recorder.h
@@ -1,5 +1,5 @@
-#ifndef debug_RECORDER_H
-#define debug_RECORDER_H
+#ifndef DEBUG_RECORDER_H
+#define DEBUG_RECORDER_H
 
 #define _USE_MATH_DEFINES
 
@@ -18,43 +18,6 @@
 #include <gnuradio/hier_block2.h>
 #include <gnuradio/io_signature.h>
 
-#include <gnuradio/analog/feedforward_agc_cc.h>
-#include <gnuradio/analog/pll_freqdet_cf.h>
-#include <gnuradio/analog/pwr_squelch_cc.h>
-#include <gnuradio/digital/diff_phasor_cc.h>
-#include <gnuradio/filter/firdes.h>
-#include <gnuradio/filter/pfb_arb_resampler_ccf.h>
-
-#if GNURADIO_VERSION < 0x030800
-#include <gnuradio/analog/sig_source_c.h>
-#include <gnuradio/blocks/multiply_cc.h>
-#include <gnuradio/blocks/multiply_const_ff.h>
-#include <gnuradio/blocks/multiply_const_ss.h>
-#include <gnuradio/filter/fir_filter_ccf.h>
-#include <gnuradio/filter/fir_filter_fff.h>
-#else
-#include <gnuradio/analog/sig_source.h>
-#include <gnuradio/blocks/multiply.h>
-#include <gnuradio/blocks/multiply_const.h>
-#include <gnuradio/filter/fir_filter_blk.h>
-#endif
-
-#include <gnuradio/block.h>
-#include <gnuradio/blocks/complex_to_arg.h>
-#include <gnuradio/blocks/copy.h>
-#include <gnuradio/blocks/short_to_float.h>
-
-#include <op25_repeater/fsk4_slicer_fb.h>
-#include <op25_repeater/gardner_costas_cc.h>
-#include <op25_repeater/include/op25_repeater/fsk4_demod_ff.h>
-#include <op25_repeater/include/op25_repeater/p25_frame_assembler.h>
-#include <op25_repeater/include/op25_repeater/rx_status.h>
-#include <op25_repeater/vocoder.h>
-
-#include <gnuradio/blocks/file_sink.h>
-#include <gnuradio/blocks/head.h>
-#include <gnuradio/message.h>
-#include <gnuradio/msg_queue.h>
 
 #include "recorder.h"
 #include "../gr_blocks/freq_xlating_fft_filter.h"
@@ -72,78 +35,30 @@ class debug_recorder;
 debug_recorder_sptr make_debug_recorder(Source *src, std::string address, int port);
 #include "../source.h"
 
-class debug_recorder : public gr::hier_block2, public Recorder {
-  friend debug_recorder_sptr make_debug_recorder(Source *src, std::string address, int port);
+class debug_recorder : virtual public gr::hier_block2, virtual public Recorder  {
+  static debug_recorder_sptr make_debug_recorder(Source *src, std::string address, int port);
 
-protected:
-  debug_recorder(Source *src, std::string address, int port);
 
 public:
-  ~debug_recorder();
+  virtual ~debug_recorder() {};
 
-  void tune_freq(double f);
-  void tune_offset(double f);
-  bool start(Call *call);
-  void stop();
-  double get_freq();
-  int get_num();
-  double get_current_length();
-  bool is_active();
-  State get_state();
-  int lastupdate();
-  long elapsed();
-  Source *get_source();
-  long get_source_count();
-  Call_Source *get_source_list();
-  void initialize_prefilter();
-  DecimSettings get_decim(long speed);
-  void generate_arb_taps();
-  //void forecast(int noutput_items, gr_vector_int &ninput_items_required);
-
-private:
-  double chan_freq;
-  double center_freq;
-  long talkgroup;
-  time_t timestamp;
-  time_t starttime;
-
-  Config *config;
-  Source *source;
-
-  //int num;
-  State state;
-
-  double system_channel_rate;
-  double arb_rate;
-  double samples_per_symbol;
-  double symbol_rate;
-
-  long decim;
-  double resampled_rate;
-  bool double_decim;
-  long if1;
-  long if2;
-  long input_rate;
-  const int phase1_samples_per_symbol = 5;
-  const double phase1_symbol_rate = 4800;
-
-  std::vector<float> arb_taps;
-  std::vector<float> sym_taps;
-  std::vector<float> baseband_noise_filter_taps;
-  std::vector<gr_complex> bandpass_filter_coeffs;
-  std::vector<float> lowpass_filter_coeffs;
-  std::vector<float> cutoff_filter_coeffs;
-
-  gr::filter::fft_filter_ccc::sptr bandpass_filter;
-  gr::filter::fft_filter_ccf::sptr lowpass_filter;
-  gr::filter::fft_filter_ccf::sptr cutoff_filter;
-
-  gr::blocks::copy::sptr valve;
-  gr::analog::sig_source_c::sptr lo;
-  gr::analog::sig_source_c::sptr bfo;
-  gr::blocks::multiply_cc::sptr mixer;
-  gr::blocks::udp_sink::sptr udp_sink;
-  gr::filter::pfb_arb_resampler_ccf::sptr arb_resampler;
+  virtual void tune_freq(double f) = 0;
+  virtual void tune_offset(double f) = 0;
+  virtual bool start(Call *call) = 0;
+  virtual void stop() = 0;
+  virtual double get_freq() = 0;
+  virtual int get_num() = 0;
+  virtual double get_current_length() = 0;
+  virtual bool is_active() = 0;
+  virtual State get_state() = 0;
+  virtual int lastupdate() = 0;
+  virtual long elapsed() = 0;
+  virtual Source *get_source() = 0;
+  virtual long get_source_count() = 0;
+  virtual Call_Source *get_source_list() = 0;
+  virtual void initialize_prefilter() = 0;
+  virtual DecimSettings get_decim(long speed) = 0;
+  virtual void generate_arb_taps() = 0;
 };
 
 #endif
--- trunk-recorder/recorders/sigmf_recorder.h
+++ trunk-recorder/recorders/sigmf_recorder.h
@@ -14,63 +14,6 @@
 #include <boost/filesystem/path.hpp>
 #include <boost/shared_ptr.hpp>
 
-#include <gnuradio/filter/firdes.h>
-#include <gnuradio/hier_block2.h>
-#include <gnuradio/io_signature.h>
-
-#include <gnuradio/analog/quadrature_demod_cf.h>
-
-#include <gnuradio/analog/agc2_cc.h>
-#include <gnuradio/analog/agc2_ff.h>
-#include <gnuradio/analog/feedforward_agc_cc.h>
-#include <gnuradio/digital/diff_phasor_cc.h>
-
-#include <gnuradio/blocks/complex_to_arg.h>
-
-#if GNURADIO_VERSION < 0x030800
-#include <gnuradio/analog/sig_source_c.h>
-#include <gnuradio/blocks/multiply_cc.h>
-#include <gnuradio/blocks/multiply_const_cc.h>
-#include <gnuradio/blocks/multiply_const_ff.h>
-#include <gnuradio/filter/fir_filter_ccf.h>
-#include <gnuradio/filter/fir_filter_fff.h>
-#include <gnuradio/filter/freq_xlating_fir_filter_ccf.h>
-#include <gnuradio/filter/rational_resampler_base_ccc.h>
-#include <gnuradio/filter/rational_resampler_base_ccf.h>
-#include <gnuradio/filter/rational_resampler_base_fff.h>
-#else
-#include <gnuradio/analog/sig_source.h>
-#include <gnuradio/blocks/multiply.h>
-#include <gnuradio/blocks/multiply_const.h>
-#include <gnuradio/filter/fir_filter_blk.h>
-#include <gnuradio/filter/freq_xlating_fir_filter.h>
-#if GNURADIO_VERSION < 0x030900
-#include <gnuradio/filter/rational_resampler_base.h>
-#else
-#include <gnuradio/filter/rational_resampler.h>
-#endif
-#endif
-
-#include <gnuradio/blocks/file_sink.h>
-#include <gnuradio/filter/pfb_arb_resampler_ccf.h>
-
-#include <gnuradio/block.h>
-#include <gnuradio/blocks/null_sink.h>
-
-#include <gnuradio/blocks/copy.h>
-
-#include <gnuradio/blocks/char_to_float.h>
-#include <gnuradio/blocks/short_to_float.h>
-
-#include <gnuradio/blocks/file_sink.h>
-#include <gnuradio/blocks/head.h>
-#include <gnuradio/message.h>
-#include <gnuradio/msg_queue.h>
-#include <op25_repeater/fsk4_demod_ff.h>
-#include <op25_repeater/fsk4_slicer_fb.h>
-#include <op25_repeater/gardner_costas_cc.h>
-#include <op25_repeater/p25_frame_assembler.h>
-#include <op25_repeater/vocoder.h>
 
 #include "recorder.h"
 #include "../gr_blocks/freq_xlating_fft_filter.h"
@@ -88,90 +31,20 @@ class sigmf_recorder;
 sigmf_recorder_sptr make_sigmf_recorder(Source *src);
 #include "../source.h"
 
-class sigmf_recorder : public gr::hier_block2, public Recorder {
-  friend sigmf_recorder_sptr make_sigmf_recorder(Source *src);
-
-protected:
-  sigmf_recorder(Source *src);
+class sigmf_recorder : virtual public gr::hier_block2, virtual public Recorder {
 
 public:
-  ~sigmf_recorder();
-
-  void tune_offset(double f);
-  bool start(Call *call);
-  void stop();
-  double get_freq();
-  int get_num();
-  double get_current_length();
-  bool is_active();
-  State get_state();
-  int lastupdate();
-  long elapsed();
-  Source *get_source();
-  long get_source_count();
-  Call_Source *get_source_list();
-
-  gr::msg_queue::sptr tune_queue;
-  gr::msg_queue::sptr traffic_queue;
-  gr::msg_queue::sptr rx_queue;
-  //void forecast(int noutput_items, gr_vector_int &ninput_items_required);
-
-private:
-  double center, freq;
-  int silence_frames;
-  long talkgroup;
-  time_t timestamp;
-  time_t starttime;
-
-  Config *config;
-  Source *source;
-  char filename[255];
-  //int num;
-  State state;
-
-  //std::vector<gr_complex> lpf_coeffs;
-  std::vector<float> lpf_coeffs;
-  std::vector<float> arb_taps;
-  std::vector<float> sym_taps;
-
-  gr::filter::freq_xlating_fir_filter_ccf::sptr prefilter;
-
-  /* GR blocks */
-  gr::filter::fir_filter_ccf::sptr lpf;
-  gr::filter::fir_filter_fff::sptr sym_filter;
-
-  gr::analog::sig_source_c::sptr lo;
-
-  gr::digital::diff_phasor_cc::sptr diffdec;
-
-  gr::blocks::multiply_cc::sptr mixer;
-  gr::blocks::file_sink::sptr fs;
-
-  gr::filter::pfb_arb_resampler_ccf::sptr arb_resampler;
-  #if GNURADIO_VERSION < 0x030900
-  gr::filter::rational_resampler_base_ccf::sptr downsample_sig;
-  gr::filter::rational_resampler_base_fff::sptr upsample_audio;
-  #else
-  gr::filter::rational_resampler_ccf::sptr downsample_sig;
-  gr::filter::rational_resampler_fff::sptr upsample_audio;
-  #endif
-  gr::analog::quadrature_demod_cf::sptr fm_demod;
-  gr::analog::feedforward_agc_cc::sptr agc;
-  gr::analog::agc2_ff::sptr demod_agc;
-  gr::analog::agc2_cc::sptr pre_demod_agc;
-  gr::blocks::file_sink::sptr raw_sink;
-  gr::blocks::short_to_float::sptr converter;
-  gr::blocks::copy::sptr valve;
-
-  gr::blocks::multiply_const_ff::sptr multiplier;
-  gr::blocks::multiply_const_ff::sptr rescale;
-  gr::blocks::complex_to_arg::sptr to_float;
-  gr::op25_repeater::fsk4_demod_ff::sptr fsk4_demod;
-  gr::op25_repeater::p25_frame_assembler::sptr op25_frame_assembler;
-
-  gr::op25_repeater::fsk4_slicer_fb::sptr slicer;
-  gr::op25_repeater::vocoder::sptr op25_vocoder;
-  gr::op25_repeater::gardner_costas_cc::sptr costas_clock;
+  virtual ~sigmf_recorder() {};
+  virtual void tune_offset(double f) = 0;
+  virtual bool start(Call *call) = 0;
+  virtual void stop() = 0;
+  virtual double get_freq() = 0;
+  virtual int get_num() = 0;
+  virtual double get_current_length() = 0;
+  virtual bool is_active() = 0;
+  virtual State get_state() = 0;
+  virtual int lastupdate() = 0;
+  virtual long elapsed() = 0;
 };
 
 #endif
