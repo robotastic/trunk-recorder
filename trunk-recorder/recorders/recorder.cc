e99914c753e9db0de95b9af196484de1c6adc066
Luke Berndt <lukekb@gmail.com>
2022-12-12 11:13:54 -0500

    Update recorder.cc

--- trunk-recorder/recorders/recorder.cc
+++ trunk-recorder/recorders/recorder.cc
@@ -9,7 +9,7 @@ Recorder::Recorder(Recorder_Type type) {
 boost::property_tree::ptree Recorder::get_stats() {
   boost::property_tree::ptree node;
   node.put("id", boost::lexical_cast<std::string>(get_source()->get_num()) + "_" + boost::lexical_cast<std::string>(get_num()));
-  node.put("type", type);
+  node.put("type", get_type_string());
   node.put("srcNum", get_source()->get_num());
   node.put("recNum", get_num());
   node.put("count", recording_count);
