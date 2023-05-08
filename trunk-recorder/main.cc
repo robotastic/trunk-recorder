42d7deb29e4fa9f191d95022a24614d17eeec269
Luke Berndt <lukekb@gmail.com>
2023-05-08 09:08:10 -0400

    Update main.cc

--- trunk-recorder/main.cc
+++ trunk-recorder/main.cc
@@ -1149,7 +1149,7 @@ void handle_call_update(TrunkMessage message, System *sys) {
 
   if (!call_found) {
     // Note: some calls maybe removed before the UPDATEs stop on the trunking channel if there is some GAP in the updates.
-     BOOST_LOG_TRIVIAL(info) << "Weird - call not found for UPDATE\tFreq: " << format_freq(message.freq) << "\tTG:" << message.talkgroup << "\tSource: " << message.source << "\tSys Num: " << message.sys_num << "\tTDMA Slot: " << message.tdma_slot << "\tTDMA: " << message.phase2_tdma;
+     BOOST_LOG_TRIVIAL(info) << "Call not found for UPDATE mesg - either we missed GRANT or removed Call too soon\tFreq: " << format_freq(message.freq) << "\tTG:" << message.talkgroup << "\tSource: " << message.source << "\tSys Num: " << message.sys_num << "\tTDMA Slot: " << message.tdma_slot << "\tTDMA: " << message.phase2_tdma;
   }
 }
 
