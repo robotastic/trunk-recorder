dad88c665b962b7396bf68c3a1703f46728605cc
Luke Berndt <lukekb@gmail.com>
2023-11-24 21:47:33 -0500

    Update sigmf_recorder_impl.cc

--- trunk-recorder/recorders/sigmf_recorder_impl.cc
+++ trunk-recorder/recorders/sigmf_recorder_impl.cc
@@ -241,8 +241,8 @@ sigmf_recorder_impl::sigmf_recorder_impl(Source *src)
   timestamp = time(NULL);
   starttime = time(NULL);
 
-  valve = gr::blocks::copy::make(sizeof(gr_complex));
-  valve->set_enabled(false);
+  initialize_prefilter();
+  initialize_prefilter_xlat();
 
   // tm *ltm = localtime(&starttime);
 
@@ -253,8 +253,7 @@ sigmf_recorder_impl::sigmf_recorder_impl(Source *src)
   }
   raw_sink = gr::blocks::file_sink::make(sizeof(gr_complex), filename);
 
-  connect(self(), 0, valve, 0);
-  connect(valve, 0, raw_sink, 0);
+  connect(fll_band_edge, 0, raw_sink, 0);
 }
 
 int sigmf_recorder_impl::get_num() {
@@ -289,6 +288,7 @@ void sigmf_recorder_impl::tune_offset(double f) {
   // have to flip this for 3.7
   // BOOST_LOG_TRIVIAL(info) << "Offset set to: " << offset_amount << " Freq: "
   //  << freq;
+    freq_xlat->set_center_freq(-f);
 }
 
 State sigmf_recorder_impl::get_state() {
@@ -315,13 +315,14 @@ bool sigmf_recorder_impl::start(Call *call) {
 
     talkgroup = call->get_talkgroup();
     freq = call->get_freq();
-
+    int offset_amount = (center_freq - chan_freq);
+    tune_offset(offset_amount);
     BOOST_LOG_TRIVIAL(info) << "sigmf_recorder.cc: Starting Logger   \t[ " << rec_num << " ] - freq[ " << freq << "] \t talkgroup[ " << talkgroup << " ]";
 
     std::stringstream path_stream;
 
     // Found some good advice on Streams and Strings here: https://blog.sensecodons.com/2013/04/dont-let-stdstringstreamstrcstr-happen.html
-    path_stream << call->get_capture_dir() << "/" << call->get_short_name() << "/" << 1900 + ltm->tm_year << "/" << 1 + ltm->tm_mon << "/" << ltm->tm_mday;
+    path_stream << call->get_temp_dir() << "/" << call->get_short_name() << "/" << 1900 + ltm->tm_year << "/" << 1 + ltm->tm_mon << "/" << ltm->tm_mday;
     std::string path_string = path_stream.str();
     boost::filesystem::create_directories(path_string);
 
