7bd48dc47038435519eed320cbfd1ccbed3e2eca
Luke Berndt <lukekb@gmail.com>
2023-12-05 09:27:29 -0500

    messing around with GRC and a C++ block that won't compile

--- trunk-recorder/gr_blocks/pfb_channelizer.h
+++ trunk-recorder/gr_blocks/pfb_channelizer.h
@@ -4,8 +4,11 @@
 #include <gnuradio/blocks/file_source.h>
 #include <gnuradio/blocks/throttle.h>
 #include <gnuradio/hier_block2.h>
-
-
+#include <gnuradio/blocks/stream_to_vector.h>
+#include <gnuradio/blocks/vector_map.h>
+#include <gnuradio/filter/filterbank_vcvcf.h>
+#include <gnuradio/fft/fft_v.h>
+#include <gnuradio/blocks/vector_to_streams.h>
 
 class pfb_channelizer : public gr::hier_block2 {
 public:
@@ -15,21 +18,26 @@ public:
   typedef std::shared_ptr<pfb_channelizer> sptr;
 #endif
 
-  static sptr make(double center,  double rate);
+  static sptr make(double center, double rate, int n_chans, int n_filterbanks, std::vector<float> taps,
+             int atten, float bw, float tb, float ripple);
          
 
-  pfb_channelizer(double center, double rate);
+  pfb_channelizer(double center, double rate, int n_chans, int n_filterbanks, std::vector<float> taps,
+             int atten, float bw, float tb, float ripple);
 
 
-  private:
+private:
+  std::vector<float> create_taps(int n_chans, int atten, float bw, float tb, float ripple);
   double d_rate;
   double d_center;
-
-    gr::blocks::file_source::sptr file_source;
-    gr::blocks::throttle::sptr throttle;
+private:
+  gr::blocks::stream_to_vector::sptr s2v;
+  gr::blocks::vector_map::sptr splitter;
+  std::vector<gr::filter::filterbank_vcvcf::sptr> fbs;
+  gr::blocks::vector_map::sptr combiner;
+  gr::fft::fft_v<gr_complex, true>::sptr fft;
+  gr::blocks::vector_map::sptr selector;
+  gr::blocks::vector_to_streams::sptr v2ss;
 };
-
-
-
 
 #endif
