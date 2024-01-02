24d07fc6634652a7aba129701549cd0cd466a2cd
Luke Berndt <luke@robotastic.com>
2024-01-02 10:00:26 -0500

    Move setting up systems out of Main.cc

---
+++ trunk-recorder/setup_systems.h
@@ -0,0 +1,22 @@
+#ifndef SETUP_SYSTEMS_H
+#define SETUP_SYSTEMS_H
+
+#include <stdlib.h>
+
+#include <gnuradio/top_block.h>
+
+#include "./global_structs.h"
+#include "call.h"
+#include "source.h"
+#include "config.h"
+#include "call_conventional.h"
+#include "systems/p25_trunking.h"
+#include "systems/smartnet_trunking.h"
+#include "systems/system.h"
+
+bool setup_conventional_channel(System *system, double frequency, long channel_index, Config &config, gr::top_block_sptr &tb, std::vector<Source *> &sources, std::vector<Call *> &calls);
+bool setup_conventional_system(System *system, Config &config, gr::top_block_sptr &tb, std::vector<Source *> &sources,  std::vector<Call *> &calls);
+bool setup_systems(Config &config, gr::top_block_sptr &tb, std::vector<Source *> &sources, std::vector<System *> &systems, std::vector<Call *> &calls);
+
+
+#endif
