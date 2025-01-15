b3d8bdc6701fc40ac0b0975dde797810bbbf9e9c
Luke Berndt <lukekb@gmail.com>
2025-01-14 22:11:24 -0500

    fixed actual error... maybe

--- trunk-recorder/call_concluder/call_concluder.cc
+++ trunk-recorder/call_concluder/call_concluder.cc
@@ -238,7 +238,13 @@ Call_Data_t upload_call_worker(Call_Data_t call_info) {
       } else {
         talkgroup_title = (char *)std::to_string(call_info.talkgroup).c_str();
       }
-      result = convert_media(call_info.filename, call_info.converted, std::ctime(&call_info.start_time), call_info.short_name.c_str(), talkgroup_title);
+      result = convert_media(
+          call_info.filename,
+          call_info.converted,
+          std::ctime(static_cast<const time_t*>(&call_info.start_time)),
+          call_info.short_name.c_str(),
+          talkgroup_title
+      );
 
       if (result < 0) {
         call_info.status = FAILED;
